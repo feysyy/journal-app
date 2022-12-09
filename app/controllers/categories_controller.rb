a4700a22c770432d211e9a01c28407ce8d7dddf1
Fey Daniel <feysy97@gmail.com>
2022-12-09 13:58:12 +0800

    Update controllers

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,54 @@
+class CategoriesController < ApplicationController
+  before_action :authenticate_user!
+  def index
+    @categories = Category.where(user_id: current_user.id) if user_signed_in?
+    @tasks_today = Task.where("date >= ? and date < ? and user_id = ?", Date.today, Date.today.tomorrow, current_user.id)
+  end
+  
+  def new
+    @category = Category.new
+  end
+
+  def show
+    @category = Category.find(params[:id])
+    @category_tasks = @category.tasks.all
+  end
+  
+  def create
+    category = Category.new(category_params)
+    category.update(user_id: current_user.id)
+
+    if category.save
+      redirect_to "/categories/#{category.id}"
+    else 
+      render :new
+    end
+  end
+
+  def edit
+    @category = Category.find(params[:id])
+  end
+
+  def update
+    category = Category.find(params[:id])
+
+    if category.update(category_params)
+      redirect_to root_path
+    else 
+      render :edit
+    end
+  end
+
+  def destroy
+    category = Category.find(params[:id])
+    category.destroy
+
+    redirect_to root_path
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name, :user_id)
+  end
+end
---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,55 @@
+class TasksController < ApplicationController
+  before_action :get_category
+
+  def new
+    @task = @category.tasks.build
+  end
+
+  def show
+    @task = Task.find(params[:id])
+    # @task = Task.where(user_id: current_user.id)
+  end
+
+  def create
+    task = @category.tasks.create(task_params)
+    task.update(user_id: current_user.id)
+
+    if task.save
+      redirect_to category_path(@category)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @task = @category.tasks.find(params[:id])
+  end
+
+  def update
+    task = @category.tasks.find(params[:id])
+
+    if task.update(task_params)
+      redirect_to category_path(@category)
+    else 
+      render :edit
+    end
+  end
+
+  def destroy
+    task = Task.find(params[:id])
+    task.destroy
+
+    redirect_to category_path(@category)
+  end
+
+  private
+
+  def get_category
+    @category = Category.find(params[:category_id])
+  end
+
+  def task_params
+    params.require(:task).permit(:name, :description, :category_id, :date, :user_id)
+  end
+
+end
